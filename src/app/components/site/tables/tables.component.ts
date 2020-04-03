2d02d80dec4954acb6bd556b1ed0db14b1c97d63
bhojim <bhojim@users.noreply.github.com>
2020-04-02 23:14:18 -0400

    Updated Tables
    Replaced html with Angular to populate the tables

--- src/app/components/site/tables/tables.component.ts
+++ src/app/components/site/tables/tables.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablesComponent implements OnInit {
 
+  heroes = [
+    { id: 1, firstName: 'Taylor', lastName: 'Swift', userName: 'tSwift' },
+    { id: 2, firstName: 'Lauren' , lastName: 'Daigle', userName: 'lDaigle'},
+    { id: 3, firstName: 'Blake' , lastName: 'Lively', userName: 'BLively'},
+    { id: 4, firstName: 'Kate' , lastName: 'Winslet', userName: 'kWinslet' },
+    { id: 5, firstName: 'Victoria' , lastName: 'Beckham', userName: 'VBeckham'},
+    { id: 6, firstName: 'Kate' , lastName: 'Hudson', userName: 'kHudson'}
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
